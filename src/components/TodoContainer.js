c60fcc6f5334b05bcd9698a0437547aca9a49f0f
SekoVper <damehlivingstone1@gmail.com>
2022-12-10 18:14:18 +0100

    Add form to create todos

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./Header";
 import TodosList from "./TodosList";
+import InputTodo from "./InputTodo"
 
 class TodoContainer extends React.Component {
   state = {
@@ -22,11 +23,43 @@ class TodoContainer extends React.Component {
       }
     ]
    };
+
+   handleChange = id => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }
+    })
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+  
    render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <InputTodo />
+        <TodosList 
+          todos={this.state.todos} 
+          handleChangeProps={this.handleChange} 
+          deleteTodoProps={this.delTodo} 
+        />
       </div>
     );
   }
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
+import TodoItem from "./TodoItem";
+
 
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
         {this.props.todos.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={this.props.handleChangeProps}
+          deleteTodoProps={this.props.deleteTodoProps}
+        />
         ))}
       </ul>
     )
