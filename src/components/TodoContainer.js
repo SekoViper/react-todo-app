67077c66e161f1e608a3fe1036a280e2ebd3d072
SekoVper <damehlivingstone1@gmail.com>
2022-12-10 11:37:01 +0100

    Add components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Header from "./Header";
+import TodosList from "./TodosList";
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+   render() {
+    return (
+      <div>
+        <Header />
+        <TodosList todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+export default TodoContainer
---
+++ src/components/TodosList.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default TodosList
