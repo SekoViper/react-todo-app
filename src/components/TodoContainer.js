f816d7b6da947b9fe135d96eaae3fe897431e113
SekoVper <damehlivingstone1@gmail.com>
2022-12-11 11:49:22 +0100

    Add methods to handle todo creations

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,23 +1,25 @@
 import React from "react"
+
 import Header from "./Header";
 import TodosList from "./TodosList";
-import InputTodo from "./InputTodo"
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -49,12 +51,24 @@ class TodoContainer extends React.Component {
       ]
     });
   };
+
+  addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
   
    render() {
     return (
       <div>
         <Header />
-        <InputTodo />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList 
           todos={this.state.todos} 
           handleChangeProps={this.handleChange} 
